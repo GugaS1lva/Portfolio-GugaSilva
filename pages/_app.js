8d6f571f05d866e3df1c51215301d34ae26d116d
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-08-05 06:26:06 -0300

    [ Ready To Code ]

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../public/styles/style.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <h1>Olá, Portfólio!</h1>
+    )
+}
+
+export default Home
