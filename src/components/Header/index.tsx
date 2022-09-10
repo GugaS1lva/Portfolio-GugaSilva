ddd2b221ef145bd91ed3ca076d6f13fe7272df64
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-09-09 23:42:30 -0300

    Menu Mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React, { useState } from 'react'
-import { FaSquareFull } from "react-icons/fa";
+import { FaSquareFull, FaBars } from "react-icons/fa";
 import {
     TemplateHeaderContainer,
     LogoContainer,
@@ -22,36 +22,35 @@ export const Header = () => {
                 </LogoContainer>
             </Link>
 
-            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-                <div className="container flex flex-wrap justify-between items-center mx-auto">
-                    <button onClick={() => setShowMenuMobile(!showMenuMobile)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <nav className="border-gray-200 absolute top-5 right-5 p-3 rounded-lg">
+                <div className="flex flex-col items-end">
+                    <button onClick={() => setShowMenuMobile(!showMenuMobile)} type="button" className="text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
+                        <FaBars className='w-7 h-7' />
                     </button>
 
-                    <div className={`${showMenuMobile ? '' : 'hidden'} w-full lg:block lg:w-auto`} id="navbar-default">
-                        <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 lg:flex-row lg:space-x-8 lg:mt-0 lg:text-sm lg:font-medium lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
+                    <div className={`${showMenuMobile ? '' : 'hidden'} lg:block lg:w-auto`}>
+                        <ul className="flex flex-col items-center justify-center py-1 rounded-lg lg:flex-row lg:text-sm lg:bg-white">
+                            <li className='pb-5'>
                                 <Link href="/">
-                                    <a className='text-black m-1 p-2 uppercase text-sm hover:text-[#0050FF]'>sobre mim</a>
+                                    <a className='font-normal text-black uppercase text-sm hover:text-[#0050FF] px-4 py-2]'>sobre mim</a>
                                 </Link>
                             </li>
 
-                            <li>
+                            <li className='pb-5'>
                                 <Link href="/curriculum">
-                                    <a className='text-black m-1 p-2 uppercase text-sm hover:text-[#0050FF]'>curriculo</a>
+                                    <a className='font-normal text-black uppercase text-sm hover:text-[#0050FF] px-4 py-2]'>curriculo</a>
                                 </Link>
                             </li>
 
-                            <li>
+                            <li className='pb-5'>
                                 <Link href="/projects">
-                                    <a className='text-black m-1 p-2 uppercase text-sm hover:text-[#0050FF]'>projetos</a>
+                                    <a className='font-normal text-black uppercase text-sm hover:text-[#0050FF] px-4 py-2]'>projetos</a>
                                 </Link>
                             </li>
 
-                            <li>
+                            <li className='pb-5'>
                                 <Link href="/contact">
-                                    <a className='text-black m-1 p-2 uppercase text-sm hover:text-[#0050FF]'>contato</a>
+                                    <a className='font-normal text-black uppercase text-sm hover:text-[#0050FF] px-4 py-2]'>contato</a>
                                 </Link>
                             </li>
                         </ul>
--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -1,9 +1,9 @@
 import tw from "tailwind-styled-components/dist/tailwind";
 
 export const TemplateHeaderContainer = tw.div`
-    flex justify-between bg-white px-9 py-8 w-full fixed z-10
+    flex justify-end items-center bg-white px-9 py-8 w-full h-[92px] fixed z-10
 `
 
 export const LogoContainer = tw.a`
-    flex items-center
+    flex flex-col items-center lg:flex lg:items-center absolute top-5 left-8 sm:top-8 sm:left-9 sm:flex-row
 `
