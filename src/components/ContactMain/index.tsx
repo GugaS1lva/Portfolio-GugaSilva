1bed7c65208b4cb36a2ad5491bc78b19fa1b7cf7
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-09-13 03:53:26 -0300

    Refactoring the Style [/contact]

--- src/components/ContactMain/index.tsx
+++ src/components/ContactMain/index.tsx
@@ -1,51 +1,69 @@
 import React from 'react'
-import { TemplateContactContainer } from './style'
 import { FaSquareFull } from "react-icons/fa";
+import { 
+    TemplateContactContainer,
+    ContactContent,
+    CentralColumn,
+    Form,
+    InputContainer,
+    InputNameContainer,
+    InputAdditionalNameContainer,
+    InputEmailContainer,
+    InputSubjectContainer,
+    InputMessageContainer,
+} from './style'
 
 export const ContactMain = () => {
     return (
         <TemplateContactContainer>
-            <main className="mt-[200px] mb-[110px]">
-                <section className='flex flex-col items-center'>
+            <ContactContent>
+                <CentralColumn>
+                    {/* ATOMIC DESIIIIIGNNNN !!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
                     <div className="flex items-center justify-center">
                         <FaSquareFull className="text-xl text-[#0050FF]" />
                         <h2 className='text-4xl font-bold font-poppins ml-2'>Contato</h2>
                     </div>
-                    <form className='font-bold text-sm bg-white py-[50px] px-[70px] my-[100px] w-[700px] shadow-[-10px_15px_10px_#00000038]'>
-                        <div className="flex mb-[60px] w-full">
-                            <div className="nameContainer w-full mr-5">
+
+                    <Form>
+                        <InputContainer>
+                            <InputNameContainer>
                                 <label className='after:content-["*"] after:text-[#0050FF] after:text-lg after:ml-1' htmlFor="iname">Nome</label>
                                 <input className='flex flex-col h-[50px] w-full border-b-2 border-[#585858] text-[#0050FF] font-bold hover:border-b-[3px] hover:border-[#0050FF] hover:text-[#0050FF] focus:border-b-[3px] focus:border-[#0050FF]' type="text" name="name" id="iname" autoComplete="name" required />
-                            </div>
-                            <div className="additionalName w-full ml-5">
+                            </InputNameContainer>
+
+                            <InputAdditionalNameContainer>
                                 <label className='after:content-["*"] after:text-[#0050FF] after:text-lg after:ml-1' htmlFor="ilname">Sobrenome</label>
                                 <input className='flex flex-col h-[50px] w-full border-b-2 border-[#585858] text-[#0050FF] font-bold hover:border-b-[3px] hover:border-[#0050FF] hover:text-[#0050FF] focus:border-b-[3px] focus:border-[#0050FF]' type="text" name="lname" id="ilname" autoComplete="additional-name" required />
-                            </div>
-                        </div>
-                        <div className="flex mb-[60px] w-full">
-                            <div className='w-full'>
+                            </InputAdditionalNameContainer>
+                        </InputContainer>
+
+                        <InputContainer>
+                            <InputEmailContainer>
                                 <label className='after:content-["*"] after:text-[#0050FF] after:text-lg after:ml-1' htmlFor="iemail">E-mail</label>
                                 <input className='flex flex-col h-[50px] w-full border-b-2 border-[#585858] text-[#0050FF] font-bold hover:border-b-[3px] hover:border-[#0050FF] hover:text-[#0050FF] focus:border-b-[3px] focus:border-[#0050FF]' type="email" name="email" id="iemal" autoComplete="email" required />
-                            </div>
-                        </div>
-                        <div className="flex mb-[60px] w-full">
-                            <div className='w-full'>
+                            </InputEmailContainer>
+                        </InputContainer>
+
+                        <InputContainer>
+                            <InputSubjectContainer>
                                 <label htmlFor="isubject">Assunto</label>
                                 <input className='flex flex-col h-[50px] w-full border-b-2 border-[#585858] text-[#0050FF] font-bold hover:border-b-[3px] hover:border-[#0050FF] hover:text-[#0050FF] focus:border-b-[3px] focus:border-[#0050FF]' type="text" name="subject" id="isubject" />
-                            </div>
-                        </div>
-                        <div className="flex mb-[60px] w-full">
-                            <div className='w-full'>
+                            </InputSubjectContainer>
+                        </InputContainer>
+
+                        <InputContainer>
+                            <InputMessageContainer>
                                 <label htmlFor="imessage">Mensagem</label>
-                                <textarea className='flex flex-col w-full h-[80%] resize-none border-b-2 border-[#585858] text-[#0050FF] font-bold hover:border-b-[3px] hover:border-[#0050FF] hover:text-[#0050FF] focus:border-b-[3px] focus:border-[#0050FF]' name="message" id="imessage" cols={30} rows={10}></textarea>
-                            </div>
-                        </div>
-                        <div className="buttonContainer">
+                                <textarea className='flex flex-col w-full h-[150px] resize-none border-b-2 border-[#585858] text-[#0050FF] font-bold hover:border-b-[3px] hover:border-[#0050FF] hover:text-[#0050FF] focus:border-b-[3px] focus:border-[#0050FF]' name="message" id="imessage" cols={30} rows={10}></textarea>
+                            </InputMessageContainer>
+                        </InputContainer>
+
+                        <div className="flex flex-col items-center sm:items-start">
                             <input className='bg-[#0050FF] text-white font-bold py-[10px] px-[40px] rounded-full mr-[10px] border-2 border-transparent hover:border-[#0050FF] hover:bg-white hover:text-black hover:cursor-pointer' type="submit" value="ENVIAR" />
                         </div>
-                    </form>
-                </section>
-            </main>
+                    </Form>
+                </CentralColumn>
+            </ContactContent>
         </TemplateContactContainer>
     )
 }
