c946c63ad035242490b6364e79994ba98d543f34
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-10-31 07:10:43 -0300

    CV Update + httpEquiv

--- src/components/MyHead/index.tsx
+++ src/components/MyHead/index.tsx
@@ -5,7 +5,7 @@ export const MyHead = () => {
     return (
         <Head>
             <meta charSet='UTF-8' />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link ref="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
